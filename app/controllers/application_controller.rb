d83c7d6c9fc2dd85add60f9bcc04f3b47f8c9df0
Badi-Haki Martin <jojackblack@gmail.com>
2023-07-27 03:15:54 -0400

    session and cookies added back in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+    include ActionController::Cookies
+
+    def hello_wurld
+        session[:count] = ( session[:count] || 0 ) + 1
+        render json: { count: session[:count] }
+    end
+
+    # end of the line here
 end
